39ee8668f1ec0280ae269a8f0c49b9a4be3a2659
Deepanshu Gurdasani <53790125+DeepanshuGurdasani@users.noreply.github.com>
2020-11-07 19:37:31 +0530

    Add files via upload
    Arpit Sironiya - Basic operation module
     Deepanshu Gurdasani - docker module , package management, team_details module and menu options module 
    Deeksha Gautam - AWS module
    Rupal Singh - networking module

---
+++ Team_details.py
@@ -0,0 +1,17 @@
+import os
+def team_details():
+    os.system("tput setaf 1")
+    os.system("clear")
+    print("*****************************************************************************************************")
+    print(""" 
+
+                                     \t Team-Task-8
+                                     \t Team- ID : ARTH2020_16_18
+                                     \t Team member: 
+                                          1 Arpit Sironiya (Team Leader) 
+                                          2 Deeksha Gautam
+                                          3 Deepanshu Gurdasani
+                                          4 Rupal Singh
+    """)
+    print("*****************************************************************************************************")
+    
